26741ae0c937e633f9c1241a528a844ea5e585c4
Victor Pinedo <victor.piendo@masglobalconsulting.com>
2022-06-23 13:31:30 -0500

    [add] ngSwitch Directive

--- src/app/components/ng-switch/ng-switch.component.ts
+++ src/app/components/ng-switch/ng-switch.component.ts
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NgSwitchComponent implements OnInit {
 
+  legends: string = "cr7";
+
   constructor() { }
 
   ngOnInit(): void {
