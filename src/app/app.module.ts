3a1bfc6a14ee874a23d5ac0d91c77bb0e5cbc4cc
Victor Pinedo <victor.piendo@masglobalconsulting.com>
2022-06-23 12:06:56 -0500

    [update] rename test to ng-if component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
-import { TestComponent } from './components/test/test.component';
+import { NgIfComponent } from './components/ng-if/ng-if.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TestComponent
+    NgIfComponent
   ],
   imports: [
     BrowserModule,
--- src/app/components/ng-if/ng-if.component.ts
+++ src/app/components/ng-if/ng-if.component.ts
@@ -1,11 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-test',
-  templateUrl: './test.component.html',
-  styleUrls: ['./test.component.scss']
+  selector: 'app-ng-if',
+  templateUrl: './ng-if.component.html',
+  styleUrls: ['./ng-if.component.scss']
 })
-export class TestComponent implements OnInit {
+export class NgIfComponent implements OnInit {
 
   showPastLegends: boolean = true;
 
