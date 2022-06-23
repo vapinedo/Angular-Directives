548780b9ef8bbaea9cde19ed68ba0a2a12887357
Victor Pinedo <victor.piendo@masglobalconsulting.com>
2022-06-23 10:22:26 -0500

    [update] readme

---
+++ src/app/components/test/test.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-test',
+  templateUrl: './test.component.html',
+  styleUrls: ['./test.component.scss']
+})
+export class TestComponent implements OnInit {
+
+  displayTitle: boolean = false;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
